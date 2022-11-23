a8626653d0196075b5af39e50b9ee249bfa00769
lamskm <30416931+lamskm@users.noreply.github.com>
2022-11-22 19:03:56 -0800

    Create overflowstr.c
    First version

---
+++ overflowstr.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{ 
+  char *source = "abcde"; // source points to memory containing "abcde\0"
+  char s1[4] = "123";     // s1[0]='1', s1[1]='2', s1[2]='3', s1[3]='\0'
+  char s2[4] = "";        // s2[0]='\0'
+  strcpy(s1,source);
+  printf("s1=%s\n",s1);
+  printf("s2=%s\n",s2);
+}
